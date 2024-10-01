0d32c76de788b98492ef8b107ed76ee769f2f45c
Mostafa Adel <mostafaadel2988@gamil.com>
2024-10-02 02:49:06 +0300

    Save changes in main.dart before switching branches

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:learnify_client/screens/SuccessPage.dart';
+import 'package:learnify_client/screens/about_us_screen/about_us.dart';
 import 'package:learnify_client/screens/bottomNav/bottom_nav.dart';
 import 'package:learnify_client/screens/change_pass_screen/cubit/validate_password_cubit.dart';
 import 'package:learnify_client/screens/create_pass_screen.dart';
@@ -11,6 +12,7 @@ import 'package:learnify_client/screens/help_center/cubit/help_center_cubit.dart
 import 'package:learnify_client/screens/profile_mentor_screen/cubit/tab_change_cubit.dart';
 import 'package:learnify_client/screens/setting_content/cubit/setting_content_cubit.dart';
 import 'package:learnify_client/screens/setting_screen/cubit/switch_cubit.dart';
+import 'package:learnify_client/screens/sign_in_screen.dart';
 
 const darkModeBox = 'darkModeTutorial';
 void main() async {
@@ -27,17 +29,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
-  final lightTheme = ThemeData(
-    brightness: Brightness.light,
-    fontFamily: 'Poppins'
-    // primaryColor: Colors.blue,
-  );
+  final lightTheme =
+      ThemeData(brightness: Brightness.light, fontFamily: 'Poppins'
+          // primaryColor: Colors.blue,
+          );
 
-  final darkTheme = ThemeData(
-    brightness: Brightness.dark,
-    fontFamily: 'Poppins'
-    // primaryColor: Colors.grey,
-  );
+  final darkTheme = ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'
+      // primaryColor: Colors.grey,
+      );
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -66,7 +65,7 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
             theme: lightTheme, // Light theme
             darkTheme: darkTheme, // Dark theme
             themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
-            home: CreatePassScreen(),
+            home: SignInScreen(),
           );
         },
       ),
