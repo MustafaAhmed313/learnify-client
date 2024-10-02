f4e003e06228f219175a3dd2bc92ae41e7afc793
Mostafa Adel <mostafaadel2988@gamil.com>
2024-10-02 20:01:15 +0300

    Save changes in main.dart before switching branches

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:learnify_client/screens/SuccessPage.dart';
+import 'package:learnify_client/screens/Verification_Code_screen.dart';
 import 'package:learnify_client/screens/about_us_screen/about_us.dart';
 import 'package:learnify_client/screens/bottomNav/bottom_nav.dart';
 import 'package:learnify_client/screens/change_pass_screen/cubit/validate_password_cubit.dart';
 import 'package:learnify_client/screens/create_pass_screen.dart';
+import 'package:learnify_client/screens/error_screen.dart';
 import 'package:learnify_client/screens/help_center/cubit/help_center_cubit.dart';
 import 'package:learnify_client/screens/profile_mentor_screen/cubit/tab_change_cubit.dart';
 import 'package:learnify_client/screens/setting_content/cubit/setting_content_cubit.dart';
@@ -66,7 +68,7 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
             theme: lightTheme, // Light theme
             darkTheme: darkTheme, // Dark theme
             themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
-            home: SignInScreen (),
+            home: ErrorScreen (),
           );
         },
       ),
